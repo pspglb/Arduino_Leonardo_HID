891a6787e202027edc8d55a8fee73bb57668a71a
Zichen Luo <RainySummerLuo@users.noreply.github.com>
2017-12-24 21:17:36 +0800

    first try

---
+++ hello_world.ino
@@ -0,0 +1,43 @@
+#include <Keyboard.h>
+#include <HID.h>
+
+void setup() {
+//   put your setup code here, to run once:
+  Keyboard.begin();
+  delay(2000);
+  Keyboard.press(KEY_LEFT_GUI); 
+  delay(500);
+  Keyboard.press('r');
+  delay(500);
+  Keyboard.release(KEY_LEFT_GUI);
+  Keyboard.release('r');
+  delay(500);
+  Keyboard.press(KEY_LEFT_CTRL);
+  delay(200);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  delay(200);
+  Keyboard.release(KEY_LEFT_CTRL);
+  Keyboard.release(KEY_LEFT_SHIFT);
+  delay(200);
+  Keyboard.press(KEY_CAPS_LOCK);
+  delay(200);
+  Keyboard.release(KEY_CAPS_LOCK);
+  delay(100);
+  Keyboard.println("cmd /t:0a");
+  delay(200);
+  Keyboard.write(0x0A);
+  delay(200);
+  Keyboard.println("echo hello word");
+  delay(200);
+  Keyboard.write(0x0A);
+  delay(200);
+  Keyboard.press(KEY_CAPS_LOCK);
+  delay(500);
+  Keyboard.release(KEY_CAPS_LOCK);
+  Keyboard.end();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
